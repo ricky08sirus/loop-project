77294bda06e69a4fa83a5bf59115583035342855
Amandeep Yadav <aman08jul@gmail.com>
2025-06-09 00:26:37 +0530

    done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,12 +4,10 @@ import dataRoute from "./routes/dataRoute";
 
 
 const app = express();
-const PORT = 4000;
+const PORT = parseInt(process.env.PORT || '4000', 10);
 app.use(cors());
 app.use("/api", dataRoute);
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running at http://0.0.0.0:${PORT}`);
 });
-
